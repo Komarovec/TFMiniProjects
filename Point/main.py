8e1dfcd7e288691ed366595ea073250ed9ba9b66
Denis <komarovec1@gmail.com>
2019-03-24 15:47:39 +0100

    New folder structure, Point example now working +/-

---
+++ Point/main.py
@@ -0,0 +1,92 @@
+
+import random
+import numpy as np
+import turtle as tt
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+
+#Generace dat
+def generateData(val):
+    data = np.empty((0,2), int)
+    for i in range(0, val):
+        posX = random.randint(-500,500)
+        posY = random.randint(-500,500)
+        data = np.append(data, [[posX,posY]], axis=0)
+    return data
+
+#Testovani dat
+def testData(data):
+    label = np.empty((0,1), int)
+    for i in data:
+        if(i[1] < 0):
+            label = np.append(label, 0)
+        else:
+            label = np.append(label, 1)
+    return label
+
+#Plotting
+def plotData(data, label):
+    point1 = (-500, 0)
+    point2 = (500, 0)
+
+    tt.hideturtle()
+    tt.pencolor("blue")
+    tt.speed(0)
+    tt.penup()
+    tt.goto(point1)
+    tt.pendown()
+    tt.goto(point2)
+    tt.penup()
+
+    for i in range(0, 100):
+        if(label[i] == 1):
+            tt.pencolor("black")
+        else:
+            tt.pencolor("red")
+        tt.penup()
+        tt.setpos(data[i]);
+        tt.pendown()
+        tt.circle(2)
+
+
+#Main
+train_data = generateData(50000)
+train_label = testData(train_data)
+
+test_data = generateData(10000)
+test_label = testData(test_data)
+
+print("Data shape:")
+print("Data shape: "+str(train_data.shape))
+print("Data: "+str(train_data))
+print("Index: "+str(train_data[0,0]))
+print("----------------")
+
+plotData(test_data, test_label)
+
+#AI
+model = keras.Sequential([ 
+    keras.layers.Dense(32, input_shape=(2,)),
+    keras.layers.Dense(4,activation=tf.nn.softmax),
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_data, train_label, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_data, test_label)
+print('Test accuracy:', test_acc)
+
+predictions = model.predict(test_data)
+print("Prediction: "+str(predictions[0].shape))
+
+predictionsTest = []
+for i in predictions:
+    predictionsTest.append(i[0])
+
+tt.clear()
+plotData(test_data, predictionsTest)
+tt.exitonclick()
---
+++ TFTutorials/BasicClassification/main.py
@@ -0,0 +1,98 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print("Data Info:")
+print("train img shape: "+str(train_images.shape))
+print("Len: "+str(len(train_labels)))
+print("Train Labels: "+str(train_labels))
+print("Test img shape: "+str(test_images.shape))
+print("Len: "+str(len(test_labels)))
+print("--------------------")
+
+
+#Normalize data
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+#Create model
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+#Compile model
+model.compile(optimizer='adam', 
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+#Train model
+model.fit(train_images, train_labels, epochs=5)
+
+#Evaluate model
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+#Make predictions
+predictions = model.predict(test_images)
+print("Prediction: "+str(predictions[0]))
+
+#Ploting functions
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+  
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1]) 
+  predicted_label = np.argmax(predictions_array)
+ 
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+#Plot results
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions, test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions, test_labels)
+plt.show()
