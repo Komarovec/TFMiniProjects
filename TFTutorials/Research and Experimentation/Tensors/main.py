ff40a5de79f7d43c44ebe13980b74673858fa7ee
Denis <komarovec1@gmail.com>
2019-05-21 17:03:26 +0200

    Tensors and Datasets

---
+++ TFTutorials/Research and Experimentation/Tensors/main.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+
+tf.enable_eager_execution()
+
+print(tf.add(1, 2))
+print(tf.add([1, 2], [3, 4]))
+print(tf.square(5))
+print(tf.reduce_sum([1, 2, 3]))
+print(tf.encode_base64("hello world"))
+
+#Operator overloading is also supported
+print(tf.square(2) + tf.square(3))
+print(tf.add([[2,3],[5,9],[7,6]], [[4,6],[6,6],[7,9]]))
+
+#Kazda matice má tvar a datový typ
+#Matice jsou neměnné !!
+x = tf.matmul([[1]], [[2, 3]])
+print(x.shape)
+print(x.dtype)
+
+
+#Numpy kompatibilita
+import numpy as np
+
+ndarray = np.ones([3, 3])
+
+print("TensorFlow operations convert numpy arrays to Tensors automatically")
+tensor = tf.multiply(ndarray, 42)
+print(tensor)
+
+
+print("And NumPy operations convert Tensors to numpy arrays automatically")
+print(np.add(tensor, 1))
+
+print("The .numpy() method explicitly converts a Tensor to a numpy array")
+print(tensor.numpy())
+
+
+#GPU / CPU
+x = tf.random_uniform([3, 3])
+
+print("Is there a GPU available: "),
+print(tf.test.is_gpu_available())
+
+print("Is the Tensor on GPU #0:  "),
+print(x.device.endswith('GPU:0'))
+
+
+#Nucene spouštění
+import time
+
+def time_matmul(x):
+  start = time.time()
+  for loop in range(10):
+    tf.matmul(x, x)
+
+  result = time.time()-start
+    
+  print("10 loops: {:0.2f}ms".format(1000*result))
+
+
+# Force execution on CPU
+print("On CPU:")
+with tf.device("CPU:0"):
+  x = tf.random_uniform([1000, 1000])
+  assert x.device.endswith("CPU:0")
+  time_matmul(x)
+
+# Force execution on GPU #0 if available
+if tf.test.is_gpu_available():
+  with tf.device("GPU:0"): # Or GPU:1 for the 2nd GPU, GPU:2 for the 3rd etc.
+    x = tf.random_uniform([1000, 1000])
+    assert x.device.endswith("GPU:0")
+    time_matmul(x)
+
+
+#Dataset Pipeline
+ds_tensors = tf.data.Dataset.from_tensor_slices([1, 2, 3, 4, 5, 6])
+
+# Create a CSV file
+import tempfile
+_, filename = tempfile.mkstemp()
+
+with open(filename, 'w') as f:
+  f.write("""Line 1
+Line 2
+Line 3
+  """)
+
+ds_file = tf.data.TextLineDataset(filename)
+
+ds_tensors = ds_tensors.map(tf.square).shuffle(2).batch(2)
+
+ds_file = ds_file.batch(2)
+
+print('Elements of ds_tensors:')
+for x in ds_tensors:
+  print(x)
+
+print('\nElements in ds_file:')
+for x in ds_file:
+  print(x)
